4b2953271a65088d1a0004917632252abc17f8a6
smallbal <605660711@qq.com>
2015-10-14 23:27:05 +0800

    完成了Ball类和Paddle类。注释中的内容有问题，球拍与球的关系没有弄好，以至于球碰到球拍就无序乱撞；当球在球拍x轴范围内则球会自动返回。

--- ping_pang.py
+++ ping_pang.py
@@ -2,7 +2,7 @@
 # -*-coding=utf-8-*-
 
 from tkinter import *
-import random
+# import random
 import time
 
 
@@ -18,17 +18,33 @@ class Ball:
         self.canvas = canvas
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
+        self.paddle = paddle
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
         self.hit_bottom = False
         self.x_pixel_of_one_step = 0  # 小球横向速度
-        self.y_pixel_of_one_step = -3  # 小球纵向速度
+        self.y_pixel_of_one_step = 3  # 小球纵向速度
 
     def hit_paddle(self, position):
         pass
 
     def draw(self):
-        pass
+        ball_pos = self.canvas.coords(self.id)
+        paddle_pos = paddle.canvas.coords(paddle.id)
+        if ball_pos[1] <= 0 or ball_pos[3] >= 500:
+            self.y_pixel_of_one_step = 0 - self.y_pixel_of_one_step
+        self.canvas.move(self.id, self.x_pixel_of_one_step, self.y_pixel_of_one_step)
+        '''
+        if ball_pos[3]>=paddle_pos[1] and self.y_pixel_of_one_step>0:
+            if ball_pos[0]>=paddle_pos[0] and ball_pos[2]<=paddle_pos[2]:
+                self.y_pixel_of_one_step = 0 - self.y_pixel_of_one_step
+                self.canvas.move(self.id, self.x_pixel_of_one_step,self.y_pixel_of_one_step)
+                return
+        if ball_pos[1]<=paddle_pos[3] and self.y_pixel_of_one_step<0:
+            if ball_pos[0]>=paddle_pos[0] and ball_pos[2]<=paddle_pos[2]:
+                self.y_pixel_of_one_step = 0 - self.y_pixel_of_one_step
+        self.canvas.move(self.id, self.x_pixel_of_one_step, self.y_pixel_of_one_step)
+        '''
 
 
 class Paddle:
@@ -37,16 +53,27 @@ class Paddle:
         self.canvas_height = canvas.winfo_height()
         self.canvas_width = canvas.winfo_width()
         self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
+        self.canvas.move(self.id, 250, 250)
         self.canvas.bind_all('<KeyPress-Right>', self.move2right)
-        self.canvas.bind_all('<KeyPress_Left>', self.move2left)
+        self.canvas.bind_all('<KeyPress-Left>', self.move2left)
         self.x_pixel_of_one_step = 0
         self.y_pixel_of_one_step = 0
 
     def move2left(self, event):
-        pass
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0:
+            self.x_pixel_of_one_step = 0
+        else:
+            self.x_pixel_of_one_step = -5
+        self.canvas.move(self.id, self.x_pixel_of_one_step, self.y_pixel_of_one_step)
 
     def move2right(self, event):
-        pass
+        pos = self.canvas.coords(self.id)
+        if pos[2] >= 500:
+            self.x_pixel_of_one_step = 0
+        else:
+            self.x_pixel_of_one_step = 5
+        self.canvas.move(self.id, self.x_pixel_of_one_step, self.y_pixel_of_one_step)
 
     def draw(self):
         pass
@@ -59,8 +86,14 @@ tk.wm_attributes('-topmost', 1)
 canvas = Canvas(tk,width=500, height=500, bd=0, highlightthickness=0)
 # canvas = Canvas(tk, width=500, height=500)
 canvas.pack()
-#tk.update()
+tk.update()
 
-#paddle = Paddle(canvas, 'black')
-#ball = Ball(canvas, 'black',)
-tk.mainloop()
+paddle = Paddle(canvas, 'black')
+ball = Ball(canvas, 'black', paddle)
+
+# tk.mainloop()
+while True:
+    ball.draw()
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(0.02)
