7cb259169d5754fc4c798c7ed699a1556498bf3e
smallbal <605660711@qq.com>
2015-10-21 23:04:28 +0800

    写入类
    完成了Sprite类， PlatformSprite类和碰撞冲突函数的编写

--- pyGame/game.py
+++ pyGame/game.py
@@ -32,9 +32,9 @@ class Game:
             if self.running:
                 for sprite in self.sprites:
                     sprite.move()
-                self.tk.update_idletasks()
-                self.tk.update()
-                time.sleep(0.01)
+            self.tk.update_idletasks()
+            self.tk.update()
+            time.sleep(0.01)
 
 
 # 坐标类
@@ -46,3 +46,84 @@ class Coords:
         self.y2 = y2
 
 
+# x轴方向检测碰撞
+def collision_x(co1, co2):
+    if co2.x1 <= co1.x1 <= co2.x2:
+        return True
+    elif co2.x1 <= co1.x2 <= co2.x2:
+        return True
+    elif co1.x1 <= co2.x1 <= co1.x2:
+        return True
+    elif co1.x1 <= co2.x2 <= co1.x2:
+        return True
+    else:
+        return False
+
+
+def collision_y(co1, co2):
+    if co2.y1 <= co1.y1 <= co2.y2:
+        return True
+    elif co2.y1 <= co1.y2 <= co2.y2:
+        return True
+    elif co1.y1 <= co2.y1 <= co1.y2:
+        return True
+    elif co1.y1 <= co2.y2 <= co1.y2:
+        return True
+    else:
+        return False
+
+
+def collision_left(colliding, collided):
+    if collision_y(colliding, collided):
+        if collided.x1 <= colliding.x1 <= collided.x2:
+            return True
+    return False
+
+
+def collision_right(colliding, collided):
+    if collision_y(colliding, collided):
+        if collided.x1 <= colliding.x2 <= collided.x2:
+            return True
+    return False
+
+
+def collision_bottom(colliding, collided, y=0):
+    if collision_x(colliding, collided):
+        y_calc = colliding.y2 + y
+        if collided.y1 <= y_calc <= collided.y2:
+            return True
+    return False
+
+
+def collision_top(colliding, collided):
+    if collision_x(colliding, collided):
+        if collided.y1 <= collided.y1 <= collided.y2:
+            return True
+    return False
+
+
+class Sprite:
+    def __init__(self, game):
+        self.game = game
+        self.end_game = False
+        self.coordinates = None
+
+    def move(self):
+        pass
+
+    def coords(self):
+        return self.coordinates
+
+
+class PlatformSprite(Sprite):
+    def __init__(self, game, photo_image, x, y, width, height):
+        Sprite.__init__(self, game)
+        self.photo_image = photo_image
+        self.image = game.canvas.create_image(x, y, image=self.photo_image, anchor='nw')
+        self.coordinates = Coords(x, y, x+width, y+height)
+
+
+g = Game()
+platform1 = PlatformSprite(g, PhotoImage(file='plateformLong.gif'), 0, 480, 100, 10)
+g.sprites.append(platform1)
+g.mainloop()
