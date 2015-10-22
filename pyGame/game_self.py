3f2c40de4e8ba503349e201155d8020f6830cac3
superman <605660711@qq.com>
2015-10-22 21:26:57 +0800

    2015/10/22 实验室 书本例子不能运行，重新写程序并保存到game_self.py

--- pyGame/game_self.py
+++ pyGame/game_self.py
@@ -18,7 +18,7 @@ class Game:
         self.tk.update()
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
-        self.bg = PhotoImage(file="./FUCK/background.gif")
+        self.bg = PhotoImage(file="background.gif")
         w = self.bg.width()
         h = self.bg.height()
         for x in range(0,5):
@@ -60,6 +60,7 @@ def collision_x(co1, co2):
         return False
 
 
+# y轴方向检测碰撞
 def collision_y(co1, co2):
     if co2.y1 <= co1.y1 <= co2.y2:
         return True
@@ -73,6 +74,7 @@ def collision_y(co1, co2):
         return False
 
 
+# 左边碰撞检测
 def collision_left(colliding, collided):
     if collision_y(colliding, collided):
         if collided.x1 <= colliding.x1 <= collided.x2:
@@ -80,6 +82,7 @@ def collision_left(colliding, collided):
     return False
 
 
+# 右边碰撞检测
 def collision_right(colliding, collided):
     if collision_y(colliding, collided):
         if collided.x1 <= colliding.x2 <= collided.x2:
@@ -87,6 +90,7 @@ def collision_right(colliding, collided):
     return False
 
 
+# 底部碰撞检测
 def collision_bottom(colliding, collided, y=0):
     if collision_x(colliding, collided):
         y_calc = colliding.y2 + y
@@ -95,6 +99,7 @@ def collision_bottom(colliding, collided, y=0):
     return False
 
 
+# 顶端碰撞检测
 def collision_top(colliding, collided):
     if collision_x(colliding, collided):
         if collided.y1 <= collided.y1 <= collided.y2:
@@ -131,7 +136,7 @@ class Man(Sprite):
             PhotoImage(file='./FUCK/kidL2.gif'),
             PhotoImage(file='./FUCK/kidL3.gif')
         ]
-        self.image_right=[
+        self.image_right = [
             PhotoImage(file='./FUCK/kidR1.gif'),
             PhotoImage(file='./FUCK/kidR2.gif'),
             PhotoImage(file='./FUCK/kidR3.gif')
@@ -141,7 +146,8 @@ class Man(Sprite):
         self.current_image = 1
         self.x = -2
         self.y = 0
-        self.jump_count = 0
+        self.jump_status = False  # 当前小人是否激发了跳跃
+        self.jump_count = 0  # 跳跃时间拍数
         self.last_time = time.time()
         self.coordinates = Coords()
         game.canvas.bind_all('<KeyPress-Left>', self.turn_left)
@@ -149,126 +155,97 @@ class Man(Sprite):
         game.canvas.bind_all('<space>', self.jump)
 
     def turn_left(self, event):
-        if self.y == 0:
-            self.x = -2
+        self.x = -2
 
     def turn_right(self, event):
-        if self.y == 0:
-            self.x = 2
+        self.x = 2
 
     def jump(self, event):
         if self.y == 0:
             self.y = -4
             self.jump_count = 0
+            self.jump.status = True
 
-    def animate(self):
-        if self.x != 0 and self.y == 0:
-            if time.time()-self.last_time>0.1:
-                self.last_time = time.time()
-                self.current_image += self.current_image_add
-                if self.current_image >= 2:
-                    self.current_image_add = -1
-                if self.current_image <= 0:
-                    self.current_image_add = 1
+    def animate(self):  # 人物动画动作设定
+        if self.x != 0 and self.y == 0 \
+                and time.time() - self.last_time > 0.1:
+            if self.current_image >= 2:
+                self.current_image_add = -1
+            elif self.current_image <= 0:
+                self.current_image_add = 1
+            else:
+                self.current_image_add = 0
         if self.x < 0:
             if self.y != 0:
                 self.game.canvas.itemconfig(self.image, image=self.image_left[2])
             else:
                 self.game.canvas.itemconfig(self.image, image=self.image_left[self.current_image])
-        if self.x > 0:
+        elif self.x > 0:
             if self.y != 0:
                 self.game.canvas.itemconfig(self.image, image=self.image_right[2])
             else:
                 self.game.canvas.itemconfig(self.image, image=self.image_right[self.current_image])
+        else:
+            if self.y == 0:
+                self.game.canvas.itemconfig(self.image, image=self.image_right[1])
 
     def coords(self):
         xy = self.game.canvas.coords(self.image)
         self.coordinates.x1 = xy[0]
         self.coordinates.y1 = xy[1]
-        self.coordinates.x2 = xy[0]+27
-        self.coordinates.y2 = xy[1]+30
+        self.coordinates.x2 = xy[2]
+        self.coordinates.y2 = xy[3]
         return self.coordinates
 
     def move(self):
         self.animate()
-        if self.y < 0:
-            self.jump_count += 1
-            if self.jump_count > 20:
-                self.y = 4
-        if self.y > 0:
-            self.jump_count -= 1
-        man_co = self.coords()
-        left = True
-        right = True
-        top = True
-        bottom = True
-        falling = True
-        # 边缘碰撞检测
-        if self.y > 0 and man_co.y1 >= self.game.canvas_height:
+        if self.jump_status:
+            if self.y <= 0:
+                self.jump_count += 1
+                if self.jump_count >= 20:
+                    self.y = 4
+            if self.y > 0:
+                self.jump_count -= 1
+                if self.jump_count <= 0:
+                    self.jump_count = 0
+                    self.jump_status = False
+        mans_co = self.coords()
+        not_bottom = True
+        not_top = True
+        not_left = True
+        not_right = True
+        # 游戏框边缘冲突检测
+        if self.y > 0 and mans_co.y2 >= self.game.canvas_height:
+            not_bottom = False
             self.y = 0
-            bottom = False
-        elif self.y < 0 and man_co.y2 <= 0:
-            self.y = 0  # 这样不会下落了吧。。。
-            top = False
-        elif self.x > 0 and man_co.x2 >= self.game.canvas_width:
+        elif self.y > 0 and mans_co.y1 <= 0:
+            not_top = False
+            self.y = 4  # 撞到游戏框顶端马上向下反弹
+        if self.x > 0 and mans_co >= self.game.canvas_width:
+            not_right = False
             self.x = 0
-            right = False
-        elif self.x < 0 and man_co.x1 <= 0:
+        elif self.x < 0 and mans_co <= 0:
+            not_left = False
             self.x = 0
-            left = False
-        # 游戏元素碰撞检测
+        else:
+            pass
+        # 小人与游戏场景道具的碰撞检测
         for sprite in self.game.sprites:
             if sprite == self:
-            # self表示“我自己”，这里即为Man类的对象，这样就不会将自己与自己进行碰撞检测了
                 continue
             sprite_co = sprite.coords()
-            if top and self.y < 0 and collision_top(man_co, sprite_co):
-                self.y = -self.y
-                top = False
-            if bottom and self.y > 0 and collision_bottom(man_co, sprite_co, self.y):
-                self.y = sprite_co.y1 - man_co.y2
-                if self.y < 0:
-                    self.y = 0
-                bottom = False
-                top = False
-            if bottom and falling and self.y == 0 \
-                    and man_co.y2 < self.game.canvas_height \
-                    and collision_bottom(man_co, sprite_co, 1):
-                falling = False
-            if left and self.x < 0 and collision_left(man_co, sprite_co):
-                self.x = 0
-                left = False
-            if right and self.x > 0 and collision_right(man_co, sprite_co):
-                self.x = 0
-                left = False
-        if falling and bottom and self.y == 0 \
-            and man_co.y2 < self.game.canvas_height:
-            self.y = 4
-        self.game.canvas.move(self.image, self.x, self.y)
+            if self.y > 0 and collision_bottom(mans_co, sprite_co, 4):
+                self.y = sprite_co.y1 - mans_co.y2
+            self.game.canvas.move(self.image, self.x, self.y)
 
 
-g = Game()
-platform1 = PlatformSprite(g, PhotoImage(file='./FUCK/plateformLong.gif'), 0, 480, 100, 10)
-'''
-platform2 = PlatformSprite(g, PhotoImage(file='./FUCK/plateformLong.gif'), 150, 440, 100, 10)
-platform3 = PlatformSprite(g, PhotoImage(file='./FUCK/plateformLong.gif'), 300, 400, 100, 10)
-platform4 = PlatformSprite(g, PhotoImage(file='./FUCK/plateformLong.gif'), 300, 160, 100, 10)
-platform5 = PlatformSprite(g, PhotoImage(file='./FUCK/plateformMiddle.gif'), 175, 350, 66, 10)
-platform6 = PlatformSprite(g, PhotoImage(file='./FUCK/plateformMiddle.gif'), 50, 300, 66, 10)
-platform7 = PlatformSprite(g, PhotoImage(file='./FUCK/plateformMiddle.gif'), 170, 120, 66, 10)
-platform8 = PlatformSprite(g, PhotoImage(file='./FUCK/plateformMiddle.gif'), 45, 60, 66, 10)
-'''
-g.sprites.append(platform1)
-'''
-g.sprites.append(platform2)
-g.sprites.append(platform3)
-g.sprites.append(platform4)
-g.sprites.append(platform5)
-g.sprites.append(platform6)
-g.sprites.append(platform7)
-g.sprites.append(platform8)
-'''
+jump_game = Game()
+platform1 = PlatformSprite(jump_game, PhotoImage(file='./FUCK/plateformLong.gif'), 0, 480, 100, 10)
+jump_game.sprites.append(platform1)
+stick_man = Man(jump_game)
+jump_game.sprites.append(stick_man)
 
-sf = Man(g)
-g.sprites.append(sf)
-g.mainloop()
+jump_game.mainloop()
+
+
+
